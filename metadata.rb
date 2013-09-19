98c6cc0437b7f5aad5f24e8fd257fe02e924e842
Chris Aumann <me@chr4.org>
2013-09-19 13:18:39 +0200

    add supported platforms (ubuntu only) to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,4 @@ license          'GNU Public License 3.0'
 description      'Installs/Configures osrm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.2'
+supports         'ubuntu'
