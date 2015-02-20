f545369933da9226181b071c40f3a694f6795d30
danyeaw <danyeaw@gmail.com>
2015-02-20 06:16:01 -0500

    Fixed path for US maps

--- attributes/regions.rb
+++ attributes/regions.rb
@@ -44,17 +44,25 @@ end
   default['osrm']['map_data'][region]['checksum'] = "#{node['osrm']['map_data'][region]['url']}.md5"
 end
 
-# north-america
+# north-american regions
 %w{
-  canada greenland us-midwest us-northeast us-pacific us-south us-west alabama alaska arizona
-  arkansas california colorado connecticut delaware district-of-columbia florida georgia hawaii
-  idaho illinois indiana iowa kansas kentucky louisiana maine maryland massachusetts michigan
-  minnesota mississippi missouri montana nebraska nevada new-hampshire new-jersey new-mexico
+  canada greenland us-midwest us-northeast us-pacific us-south us-west
+}.each do |region|
+  default['osrm']['map_data'][region]['profiles'] = %w{car}
+  default['osrm']['map_data'][region]['url'] = "http://download.geofabrik.de/north-america/#{region}-latest.osm.pbf"
+  default['osrm']['map_data'][region]['checksum'] = "#{node['osrm']['map_data'][region]['url']}.md5"
+end
+
+# us states
+%w{
+  alabama alaska arizona arkansas california colorado connecticut delaware district-of-columbia florida
+  georgia hawaii idaho illinois indiana iowa kansas kentucky louisiana maine maryland massachusetts
+  michigan minnesota mississippi missouri montana nebraska nevada new-hampshire new-jersey new-mexico
   new-york north-carolina north-dakota ohio oklahoma oregon pennsylvania rhode-island south-carolina
   south-dakota tennessee texas utah vermont virginia washington west-virginia wisconsin wyoming
 }.each do |region|
   default['osrm']['map_data'][region]['profiles'] = %w{car}
-  default['osrm']['map_data'][region]['url'] = "http://download.geofabrik.de/north-america/#{region}-latest.osm.pbf"
+  default['osrm']['map_data'][region]['url'] = "http://download.geofabrik.de/north-america/us/#{region}-latest.osm.pbf"
   default['osrm']['map_data'][region]['checksum'] = "#{node['osrm']['map_data'][region]['url']}.md5"
 end
 
