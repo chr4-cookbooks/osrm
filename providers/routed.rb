7f52ee74b181d0b61eab378b6038ff40bac3a43f
Martin Bokman <martin@bokman.org>
2015-07-06 17:35:58 +0200

    Two small fixes, one document change.
    1. The upstart job needs ’-c’ for the configuration file argument.
    2. Package libtbb-dev needs to be installed or there will be
    compilation errors (Ubuntu 14.04).
    3. I believe the Project OSRM links are incorrect due to an earlier
    pull request.

--- providers/routed.rb
+++ providers/routed.rb
@@ -64,7 +64,7 @@ action :create do
     source    'upstart.conf.erb'
     cookbook  'osrm'
     variables description: 'OSRM route daemon',
-              daemon:      "#{daemon} #{config_file} #{map_file}",
+              daemon:      "#{daemon} -c #{config_file} #{map_file}",
               user:        user
   end
 
